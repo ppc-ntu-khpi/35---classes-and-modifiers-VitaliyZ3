2be3aec8c4a5fd2c69556c171664e1bf210c341c
VitaliyZ3 <72409864+VitaliyZ3@users.noreply.github.com>
2020-11-02 22:12:51 +0200

    Add files via upload

---
+++ src/CustomerTest.java
@@ -0,0 +1,11 @@
+public class CustomerTest {
+
+	public static void main(String[] args) {
+		Сustomer c = new Сustomer();
+    c.setID(4);
+    c.setNew(false);
+    c.setTotal(313.24);
+    c.displayCustomerInfo();
+	}
+
+}

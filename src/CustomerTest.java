dc49e9b03b56de997a09e5246b41aa292f9d2ef1
VitaliyZ3 <vitasiamen@gmail.com>
2020-11-02 20:30:25 +0000

    I change anything

--- src/CustomerTest.java
+++ src/CustomerTest.java
@@ -2,9 +2,9 @@ public class CustomerTest {
 
 	public static void main(String[] args) {
 		Сustomer c = new Сustomer();
-    c.setID(4);
+    c.setID(1);
     c.setNew(false);
-    c.setTotal(313.24);
+    c.setTotal(1.1);
     c.displayCustomerInfo();
 	}
 
